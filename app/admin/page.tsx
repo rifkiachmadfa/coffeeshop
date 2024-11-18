c69ca1b6624551f8834479bb4508618f278bb345
mufqiuwais <taufan.rizki123@gmail.com>
2024-11-18 22:00:10 +0700

    updatepageadmin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,10 @@
-export default function Admin (){
-    return(
+export default function Admin() {
+    return (
         <>
-        <h1>ini admin</h1>
-        <p>isi defaultnya daftar menu yang tersedia, dapat diedit dan dihapus.</p>
-        <p>ada menu navigasi terdiri dari thumbnail, pesanan, tambah voucher, dan logout</p>
+            <h1>ini jokowi</h1>
+            <p>isi defaultnya daftar menu yang tersedia, dapat diedit dan dihapus.</p>
+            <p>ada menu navigasi terdiri dari thumbnail, pesanan, tambah voucher, dan logout</p>
+
         </>
     )
 }
