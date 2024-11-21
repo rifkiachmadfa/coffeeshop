b6c054470e5f07eefeddb623db3b0f4ee8ddf6d9
YugoPratama <yugo.pratama@widyatama.ac.id>
2024-11-21 23:40:18 +0700

    apa aja

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-      <h1>ini home alone</h1>
-=======
-    <h1>ini home alonesss</h1>
->>>>>>> edce66e0be7f86239916571d62b4b1217bdec09d
+      <h1>ini home alonesss</h1>
     </>
   );
 }
