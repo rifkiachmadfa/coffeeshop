b95a7fac906f743a203e9bd490febd63ef5e48c8
Rifki Achmad Fachrezy <rifkiachmadfachrezy@gmail.com>
2024-11-24 21:54:23 +0700

    final

--- app/admin/tambahMenu/page.tsx
+++ app/admin/tambahMenu/page.tsx
@@ -1,7 +1,7 @@
 const TambahMenu = () => {
   return (
     <>
-      <h1>Ini fitur dari branch admin Tambah Menu final</h1>
+      <h1>Ini fitur dari branch admin Tambah Menu finals</h1>
       <p>hahahahahha</p>
     </>
   );
