12c533a82ddc0ebb6954e29e8c959bedaa083ef5
alvin <alvin.gustafianto@widyatama.ac.id>
2024-11-24 23:19:42 +0700

    voucher

--- app/admin/voucher/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Admin (){
-    return(
-        <>
-        <h1>ini tambah voucher</h1>
-        <p>isi voucher yang aktif</p>
-        <p>dapat mengedit voucher dan menghapus serta menambah voucher</p>
-        </>
-    )
-}
