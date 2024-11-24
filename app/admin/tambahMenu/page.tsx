b4f167be7f1f6526352d5cbd6ae0be62d3ef31e7
Rifki Achmad Fachrezy <rifkiachmadfachrezy@gmail.com>
2024-11-24 21:51:16 +0700

    final fitur tes

--- app/admin/tambahMenu/page.tsx
+++ app/admin/tambahMenu/page.tsx
@@ -1,11 +1,8 @@
 const TambahMenu = () => {
   return (
     <>
-      <h1>Ini fitur dari branch admin Tambah Menu</h1>
-      <p>
-        Isinya form terdiri dari nama produk, harga, attachment gambar,
-        deskripsi, dan status tersedia atau tidak.
-      </p>
+      <h1>Ini fitur dari branch admin Tambah Menu final</h1>
+      <p>hahahahahha</p>
     </>
   );
 };
