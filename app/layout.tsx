4208bdd94b4897fe4ac4b0f45c552a9ab85810aa
rifki achmad fachrezy <rifkiachmadfachrezy@gmail.com>
2024-12-10 14:52:13 +0700

    penambahan fitur login register terintegrasi ke database

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex justify-center">
-        <main className="antialiased w-[1280px] h-[100vh]">
+        <main className=" w-[1280px] h-[100vh]">
           <div className=""></div>
 
           <div className="">
