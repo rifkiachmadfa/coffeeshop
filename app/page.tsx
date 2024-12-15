8c8af55d0ab402f9a67d806d1b6589e579c452b2
Anggitamanik <anggita.manik@widyatama.ac.id>
2024-12-15 21:54:54 +0700

    Update Menu

--- app/page.tsx
+++ app/page.tsx
@@ -19,14 +19,14 @@ export default function Home() {
         <div className="relative z-1  justify-center  top-[500px] pt-40 bg-white rounded-tr-[100px] rounded-tl-[100px]">
           <p>fitur shorting</p>
           <div className="flex justify-center grid grid-cols-4 gap-8 mx-20">
-            <CardListProduk namaProduk="aren Latte" deskripsi="kopi mantap" />
-            <CardListProduk namaProduk="matcha latte" deskripsi="original matcha" />
-            <CardListProduk namaProduk="americano" deskripsi="kopi enak" />
-            <CardListProduk />
-            <CardListProduk />
-            <CardListProduk />
-            <CardListProduk />
-            <CardListProduk />
+            <CardListProduk namaProduk="Aren Latte" deskripsi="Kopi Susu Aren" />
+            <CardListProduk namaProduk="Brave Latte" deskripsi="Kopi Susu Lembut dan Kental" />
+            <CardListProduk namaProduk="Caffe Americano" deskripsi="Kopi Rasa Espresso" />
+            <CardListProduk namaProduk="Cappuccino" deskripsi="Espresso Sempurna Dengan Susu"/>
+            <CardListProduk namaProduk="Caffe Mocha" deskripsi="Espresso dan Cokelat Ganache"/>
+            <CardListProduk namaProduk="Timiri Oat Latte" deskripsi="Minuman Nge-hits Kita"/>
+            <CardListProduk namaProduk="Caramel Macchiato" deskripsi="Espresso Dengan Sirup Karamel" />
+            <CardListProduk namaProduk="Caffe Flat White" deskripsi="Cafe Latte Ala New Zealand"/>
           </div>
         </div>
       </Template>
